d975af22f77201d4038935200461cbb21d51619b
Apple <apple@Apples-MBP-63.attlocal.net>
2019-07-12 18:16:07 -0400

    Complete Iteration 0

---
+++ testToDoList/ToDo.swift
@@ -0,0 +1,20 @@
+//
+//  ToDo.swift
+//  testToDoList
+//
+//  Created by Apple on 7/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+    
+//    init(toDoName : String, todoImportant : Bool) {
+//        name = toDoName
+//        important = todoImportant
+//    }
+    
+}
